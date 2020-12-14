6f64b96a00d44010f69f15cbccc733760d09dcd9
Oleksiy Bondar <bondaroelskiyandriyovich@gmail.com>
2020-12-14 17:51:45 +0200

    code comments added

--- yet-another-framework/lib/config.rb
+++ yet-another-framework/lib/config.rb
@@ -1,11 +1,22 @@
-module YetAnotherFramework
-  module Config
-    @@wait_timeout = 5 # secconds
 
+module YetAnotherFramework
+  # going to contains static Framework configuration fields
+  #
+  module Config
+    @@wait_timeout = 5 # seconds
+
+    # wait timeout getter
+    #
+    # @return [Numeric] - timeout in seconds
+    #
     def self.wait_timeout
       @@wait_timeout
     end
 
+    # wait timeout setter
+    #
+    # @param [Numeric] value - a new timeout value
+    # @return [Numeric]
     def self.wait_timeout=(value)
       @@wait_timeout = value
     end
